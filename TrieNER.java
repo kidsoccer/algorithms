3969a2b3a192ba1cec73bf13e21aa5e34d6f570b
xuanthanh87 <theblue20187@gmail.com>
2019-02-07 17:05:19 +0700

    Update TrieNER.java
    add method to check n gram against to the dictionary

--- TrieNER.java
+++ TrieNER.java
@@ -1,11 +1,6 @@
 package com.sentifi.greenflow;
 
-import com.google.common.collect.ImmutableList;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TrieNER {
@@ -14,7 +9,7 @@ public class TrieNER {
 
         final Map<String,Trie> dictionary = new HashMap<>();
 
-        List<String> inputDic = ImmutableList.of(
+        List<String> inputDic = Arrays.asList(new String []{
                 "Digitisation",
                 "Axon Active",
                 "Axon Lover",
@@ -32,15 +27,59 @@ public class TrieNER {
                 "Shanghai Stock Exchange Composite Index",
                 "Shanghai Stock Exchange Composite Index",
                 "OnVista Group"
-        );
+        });
         for (String word: inputDic
-                ) {
+        ) {
             buildTrie(word,dictionary);
         }
 
-        System.out.println(isInDic("Morelia",dictionary));
+        System.out.println(isInDic("Donal",dictionary));
         System.out.println(isInDic("Surgical Care Affiliates",dictionary));
         System.out.println(isInDic("Stock Exchange",dictionary));
+
+        replaceNGram("Donal Trump OnVista Group hello axon active hate axon lover but he likes Morelia very much ",dictionary);
+    }
+
+    static List<String> replaceNGram(String sentence,Map<String,Trie> dictionary){
+
+        List<String> result = new ArrayList<>();
+
+        String [] arr = sentence.split(" ");
+        for (int i = 0; i< arr.length;i++){
+                String prefix = arr[i];
+
+                int count = i;
+                while(count< arr.length -1) {
+                    if (Objects.nonNull(isInDic(prefix, dictionary))){
+                        result.add(prefix);
+                }
+
+
+                    count ++;
+                    prefix = prefix + " " + arr[count ];
+
+
+                }
+
+
+        }
+        return result;
+
+    }
+
+    static String   isInDic(List<String> words,Map<String,Trie> dictionary){
+        //String [] wordArr = words.toLowerCase().split(" ");
+        String prefix = words.get(0);
+        List<String> children =  words.stream().skip(1).collect(Collectors.toList());
+        if (dictionary.containsKey(prefix)){
+            if(!children.isEmpty()) {
+                Trie trie = dictionary.get(prefix);
+                return
+                        trie.find(children);
+            }
+            return prefix;
+        }
+        return null;
     }
 
     static String isInDic(String words,Map<String,Trie> dictionary){
@@ -53,7 +92,13 @@ public class TrieNER {
                 return
                         trie.find(children);
             }
-            return prefix;
+
+            //node with empty children
+            Trie trie = dictionary.get(prefix);
+            if(trie.terminal){
+                return trie.value;
+            }
+
         }
         return null;
     }
